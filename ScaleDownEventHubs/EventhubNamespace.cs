5b8517501cc2fa5abc7f241208e64f0a3e2d3d20
Darel Johnson <darel.johnson@improving.com>
2022-09-06 10:10:11 -0400

    First commit

---
+++ ScaleDownEventHubs/EventhubNamespace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScaleDownEventHubs
+{
+    internal class EventhubNamespace
+    {
+        public EventhubNamespace(string subscriptionId, string resourceGroup, string @namespace, int targetThroughputUnits)
+        {
+            SubscriptionId = subscriptionId;
+            ResourceGroup = resourceGroup;
+            Namespace = @namespace;
+            TargetThroughputUnits = targetThroughputUnits;
+        }
+
+        public string SubscriptionId { get; set; }
+        public string ResourceGroup { get; set; }
+        public string Namespace { get; set; }
+        public int TargetThroughputUnits { get; set; }
+    }
+}
